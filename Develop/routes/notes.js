65f58ce36c939a5da4e94e2e02ebe84f728b1aeb
Lucy Kwon <ks02ks222@gmail.com>
2024-01-22 00:43:02 -0800

    added post suite for notes router

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -10,3 +10,29 @@ notes.get('/', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
+//POST route for creating a new note
+notes.post('/', (req,res) => {
+    //Deconstructing assignment for the items in req.body
+    const { title, text } = req.body;
+    //if all of the required properties are in the req.body, create a new note
+    if ( title && text) {
+        const newNote = {
+            title,
+            text,
+            note_id: uuidv4(),
+        };
+        //updating db.json by appending the newly created note
+        readAndAppend(newNote, './db/db.json');
+        //create a new response object with a status 
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+        //send it as a response
+        res.json(response);
+    } else {
+        res.json('Error in posing a new note');
+    }
+});
+//exporting notes router.
+module.exports = notes;
