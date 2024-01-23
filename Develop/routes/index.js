bac70be57b91bb82e01fefb33ea7787977bf0ddc
Lucy Kwon <ks02ks222@gmail.com>
2024-01-23 00:15:10 -0800

    added comments for the index.js

--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -1,7 +1,8 @@
+//import express and router method
 const router = require('express').Router();
-
+//create a notesRouter
 const notesRouter = require('./notes');
-
+//use the notesRouter when endpoint is api/notes in the request
 router.use('/notes',notesRouter);
-
+//export
 module.exports = router;
